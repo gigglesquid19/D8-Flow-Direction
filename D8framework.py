6f9b6df7b6d36eb451120ceed1a7040d282bcd74
gigglesquid19 <67693120+gigglesquid19@users.noreply.github.com>
2021-05-21 11:25:46 +0100

    Add files via upload

--- D8framework.py
+++ D8framework.py
@@ -7,8 +7,14 @@ Created on Mon May 17 16:00:08 2021
 
 import math
 
-# Finds the pixel with the largest % elev change, returns pixid and elevation deduct
 def whichpix(l, k, elev_copy, pixellist):
+    '''
+    Takes in the values of l and k and elev_copy dataset, along with list of
+    neighboring pixels with values. Loops through the pixellist and finds the
+    and calculates a droplist of the differences between neighboring pixel
+    elevations and original pixel elevation. Next, finds the lowest drop value, 
+    the elevdeduct and notes the pixelid, returning both.
+    '''
     droplist = []
     for pixel in pixellist:
         drop = pixel - elev_copy[l][k]
@@ -24,7 +30,15 @@ def whichpix(l, k, elev_copy, pixellist):
         
 # Flow Direction function uses variables to calc largest % change in elev
 # and updates the pixel to return the directional code          
-def flowdirection(l, k, diag, elev_copy, pixellist, pixid , elevdeduct):           
+def flowdirection(l, k, diag, elev_copy, pixellist, pixid , elevdeduct): 
+    '''
+    Takes in the value of l and k and the elev_copy dataset, along with the
+    elededuct, pixid returned from whichpix function and the diagonal boolean
+    and pixellist. Depending on the pixelid and boolean, the function calc 
+    whether pixel is orthogonal or diagonal from original pixel and uses
+    formula to calculate the max_drop, updating the pixel. 
+    It also returns the flow direction value.
+    '''          
             
     # Check value of pixelid to determine direction
     if pixid == 0:
@@ -110,4 +124,8 @@ def flowdirection(l, k, diag, elev_copy, pixellist, pixid , elevdeduct):
             return 128
         
 def update(l, k, elev_copy, directcode):
+    '''
+    This simple function updates the pixel value with the directional code
+    returned from the flow direction function
+    '''
     elev_copy[l][k] = directcode
